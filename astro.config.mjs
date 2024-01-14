2931c803d80baed61b4bc0e08454465c9751c1a2
Shauryaditya <shauryadityab@gmail.com>
2024-01-14 08:14:50 +0530

    serverless error fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,18 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-
+import vercelServerless from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/serverless';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind()],
+  output: 'server',
+  adapter: vercelServerless(),
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+    maxDuration: 8,
+  }),
 });
