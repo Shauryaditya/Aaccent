24f6b62242cf0767938788c6baa6d044fc370aa3
Shauryaditya <shauryadityab@gmail.com>
2024-01-29 12:56:35 +0530

    google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,20 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import mdx from '@astrojs/mdx';
+import partytown from '@astrojs/partytown'
+
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [react(), tailwind(),
+    mdx(), 
+		sitemap(),
+		partytown({
+			config: {
+			  forward: ["dataLayer.push"],
+			},
+		}),
+  ],
 });
